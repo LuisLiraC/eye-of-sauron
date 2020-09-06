83e153ba430653cfef23108430887a7e6aca838f
LuisLiraC <luis_ssbm@hotmail.com>
2020-09-06 11:25:47 -0500

    [fix] welcome

--- src/index.js
+++ src/index.js
@@ -10,17 +10,17 @@ client.login(config.token)
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.cache.find(ch => ch.name === '🖖-welcome')
   if (!channel) return
-  const emoji = this.client.emojis.cache.get('724816411733786808')
+  const emoji = client.emojis.cache.get('724816411733786808')
   channel.send(`¡Hola ${member}! Bienvenvid@ a la comunidad de Undefined Devs ${emoji}`)
 })
 
 client.on('message', message => {
   if (!message.guild) return
   if (message.author.bot) return
-
+  
   const validator = new Validator()
   const id = message.author.id
-
+  
   if (validator.isUndefinedDev(id) && message.content.startsWith('!')) {
     const dispatcher = new Dispatcher(client)
     const messageStart = message.content.replace(/(![a-zA-Z]{0,9}) .*/, '$1')
